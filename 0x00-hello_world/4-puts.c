ccb7161fcd16bda7fc11a0b87ea4367aa86e016a
abduRahman94 <sabdouseye@gmail.com>
2024-06-28 02:59:42 -0700

    Print message to stdout

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/*
+/**
  * main - Entry point
  * puts - Prints argument to standard output adding a trailing new line
  *
@@ -8,5 +8,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
