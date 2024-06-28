528027973cb5cdfa3ddced28ab45aba52bac85b2
abduRahman94 <sabdouseye@gmail.com>
2024-06-28 03:33:27 -0700

    Proper grammar

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *
+ * main - Entry point
+ *
+ * Description: Prints a message to standard output with a trailing line.
+ * It uses printf function
+ * Return: Always o (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
