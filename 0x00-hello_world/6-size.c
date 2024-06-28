074c1e4cbe1becc529e2c99f2e3fcc59cdd32722
abduRahman94 <sabdouseye@gmail.com>
2024-06-28 08:55:48 -0700

    type printers

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Program that display the size of different types
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a Char: %c bytes\n", sizeof(char));
+	printf("Size of an int: %d bytes\n", sizeof(int));
+	printf("Size of a long int: %ld bytes\n", sizeof());
+	printf("Size of a long long int: %lld bytes\n", sizeof());
+	printf("Size of a float: %f bytes\n", sizeof());
+}
