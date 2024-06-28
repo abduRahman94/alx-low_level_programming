13a088b4db6babf14b8151dd133cfa9290f4a0b0
abduRahman94 <sabdouseye@gmail.com>
2024-06-28 09:03:09 -0700

    type printers

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,9 +8,10 @@
  */
 int main(void)
 {
-	printf("Size of a Char: %c bytes\n", sizeof(char));
-	printf("Size of an int: %d bytes\n", sizeof(int));
-	printf("Size of a long int: %ld bytes\n", sizeof(long int));
-	printf("Size of a long long int: %lld bytes\n", sizeof(long long int));
-	printf("Size of a float: %f bytes\n", sizeof(float));
+	printf("Size of a char: %zu bytes\n", sizeof(char));
+	printf("Size of an int: %zu bytes\n", sizeof(int));
+	printf("Size of a long int: %zu bytes\n", sizeof(long int));
+	printf("Size of a long long int: %zu bytes\n", sizeof(long long int));
+	printf("Size of a float: %zu bytes\n", sizeof(float));
+	return (0);
 }
