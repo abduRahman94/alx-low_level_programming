96988b06c463c090f26afdb701f05a0266c3506e
abduRahman94 <sabdouseye@gmail.com>
2024-07-07 08:54:10 -0700

    Std output issue

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,17 +4,22 @@
 
 /**
  * main - Entry point
- * Return : 0 Always (Success)
+ *
+ * return : 0 Always (Success)
  */
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if(n > 0) {
+	if (n > 0) 
+	{
 		printf("%d is positive\n", n);
 	}
-	else if(n < 0) {
+
+	else if (n < 0) 
+	{
 		printf("%d is negative\n", n);
 	}
 	else {
