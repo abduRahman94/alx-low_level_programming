e5d72b9f2047ac6f8dcc9fa569b6d4bee45e3625
abduRahman94 <sabdouseye@gmail.com>
2024-07-08 15:10:04 -0700

    Three digits together

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,16 +10,16 @@ int main(void)
 	int second_iter;
 	int third_iter;
 
-	for (first_iter = 0; first_iter <= 8; first_iter++)
+	for (first_iter = 0; first_iter <= 7; first_iter++)
 	{
-		for (second_iter = first_iter + 1; second_iter <= 9; second_iter++)
+		for (second_iter = first_iter + 1; second_iter <= 8; second_iter++)
 		{
 			for (third_iter = second_iter + 1; third_iter <= 9; third_iter++)
 			{
 				putchar(first_iter + '0');
 				putchar(second_iter + '0');
 				putchar(third_iter + '0');
-				if (first_iter != 8 || second_iter != 9 || third_iter != 9)
+				if (first_iter != 7 || second_iter != 8 || third_iter != 9)
 				{
 					putchar(',');
 					putchar(' ');
