eeedc2cfe48dc561dfa51b99c4f070925681566f
abduRahman94 <sabdouseye@gmail.com>
2024-07-08 16:56:12 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,16 +3,16 @@
 /**
  * main - check code
  *
+ * print_alphabet - Prints the alphabet letters to stdout followed by empty line
  * Return: Always 0.
  */
-
 void print_alphabet(void)
 {
         char letter;
 
 	for (letter = 'a'; letter <= 'z'; letter++)
-        {
-                _putchar(letter);
-        }
-        _putchar('\n');
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
 }
