e2152952637b57098f156832e15b7cf1d80ded04
abduRahman94 <sabdouseye@gmail.com>
2024-07-08 15:31:32 -0700

    Four digits together

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,28 +8,20 @@ int main(void)
 {
 	int first_iter;
 	int second_iter;
-	int third_iter;
-	int fourth_iter;
 
-	for (first_iter = 0; first_iter <= 6; first_iter++)
+	for (first_iter = 0; first_iter <= 98; first_iter++)
 	{
-		for (second_iter = first_iter + 1; second_iter <= 7; second_iter++)
+		for (second_iter = first_iter + 1; second_iter <= 99; second_iter++)
 		{
-			for (third_iter = second_iter + 1; third_iter <= 8; third_iter++)
+			putchar((first_iter / 10) + '0');
+			putchar((first_iter % 10) + '0');
+			putchar(' ');
+			putchar((second_iter / 10) + '0');
+			putchar((second_iter % 10) + '0');
+			if (first_iter != 98 || second_iter != 99)
 			{
-				for (fourth_iter = third_iter + 1; fourth_iter <= 9; fourth_iter++)
-				{
-					putchar(first_iter + '0');
-					putchar(second_iter + '0');
-					putchar(' ');
-					putchar(third_iter + '0');
-					putchar(fourth_iter + '0');
-					if (first_iter != 6 || second_iter != 7 || third_iter != 8 || fourth_iter != 9)
-					{
-						putchar(',');
-						putchar(' ');
-					}
-				}
+				putchar(' ');
+				putchar(',');
 			}
 		}
 	}
