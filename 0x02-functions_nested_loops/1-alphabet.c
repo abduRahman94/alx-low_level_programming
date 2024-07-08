f1df0ca88c48c4e49e72c6bee687f4a0395eb4a4
abduRahman94 <sabdouseye@gmail.com>
2024-07-08 16:44:46 -0700

    function print alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,6 +5,16 @@
  *
  * Return: Always 0.
  */
+
+void print_alphabet(void):
+{
+        for (char letter = 'a'; letter <= 'z'; letter++)
+        {
+                _putchar(letter);
+        }
+        _putchar('\n');
+}
+
 int main(void)
 {
 	print_alphabet();
