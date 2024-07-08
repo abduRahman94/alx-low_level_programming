07119bbb37143a74b01734b883c451682ae9f51c
abduRahman94 <sabdouseye@gmail.com>
2024-07-08 06:26:16 -0700

    Print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int dg;
+	char ch;
+
+	for (dg = 0; dg <= 9; dg++)
+	{
+		putchar(dg);
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
