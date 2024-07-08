368d671d60ee725c13567d5ec772400c683d4379
abduRahman94 <sabdouseye@gmail.com>
2024-07-08 14:53:08 -0700

    Print digit pairs

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,13 +9,13 @@ int main(void)
 	int first_iter;
 	int second_iter;
 
-	for (first_iter = 0; first_iter <= 9; first_iter++)
+	for (first_iter = 0; first_iter <= 8; first_iter++)
 	{
-		for (second_iter = 0; second_iter <= 9; second_iter++)
+		for (second_iter = first_iter + 1; second_iter <= 9; second_iter++)
 		{
-			putchar(first_iter);
-			putchar(second_iter);
-			if (second_iter < 9)
+			putchar(first_iter + '0');
+			putchar(second_iter + '0');
+			if (first_iter != 8 || second_iter != 9)
 			{
 				putchar(',');
 				putchar(' ');
