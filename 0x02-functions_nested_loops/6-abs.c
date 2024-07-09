b0719283be38c3274751dc529fc54974e5d63cb0
abduRahman94 <sabdouseye@gmail.com>
2024-07-08 18:30:20 -0700

    Absolute value

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /**
- * print_sign - Returns sign of given argument
+ * abs - Returns absolute value of given argument
  * @c: the char to check
  * Return: Always 0.
  */
 int _abs(int c)
 {
-	return abs(c);
+	return (abs(c));
 }
