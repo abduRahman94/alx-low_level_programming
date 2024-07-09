4b298ddbe475b0849c2321b0d56b62f1bf5d1cfc
abduRahman94 <sabdouseye@gmail.com>
2024-07-08 19:19:27 -0700

    Display numbers

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_last_digit - Returns last digit of given number
+ * @c: the char to check
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	int hours;
+	int mins;
+
+	for (hours = 0; hours <= 23; hours++)
+	{
+		for (mins = 0; mins <= 59; mins++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((minutes / 10) + '0');
+			_putchar((minutes % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
