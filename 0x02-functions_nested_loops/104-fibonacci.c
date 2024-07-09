5f681f0c3b772d2d4c84950962650267b8667bf4
abduRahman94 <sabdouseye@gmail.com>
2024-07-09 01:19:23 -0700

    updated fibonacci sequence

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,20 +7,44 @@
  */
 int main(void)
 {
-	long int a = 1, b = 2, next;
-	long counter = 0;
+    unsigned long a = 1, b = 2;
+    unsigned long a_high, a_low, b_high, b_low;
+    unsigned long high, low;
+    int count;
 
-	while (counter <= 98)
-	{
-		next = a + b;
+    printf("%lu, %lu", a, b);
 
-		if (counter != 98)
-			printf("%ld, ", a);
+    for (count = 2; count < 92; count++)
+    {
+        unsigned long next = a + b;
+        printf(", %lu", next);
+        a = b;
+        b = next;
+    }
 
-		a = b;
-		b = next;
-		counter++;
-	}
+    a_high = a / 10000000000UL;
+    a_low = a % 10000000000UL;
+    b_high = b / 10000000000UL;
+    b_low = b % 10000000000UL;
 
-	return (0);
+    for (; count < 98; count++)
+    {
+        high = a_high + b_high;
+        low = a_low + b_low;
+        if (low >= 10000000000UL)
+        {
+            high += 1;
+            low %= 10000000000UL;
+        }
+
+        printf(", %lu%010lu", high, low);
+
+        a_high = b_high;
+        a_low = b_low;
+        b_high = high;
+        b_low = low;
+    }
+
+    printf("\n");
+    return (0);
 }
