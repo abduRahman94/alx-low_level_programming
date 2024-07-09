f2057e23f303cc2127e42f3844b6de36753de9f9
abduRahman94 <sabdouseye@gmail.com>
2024-07-08 19:46:40 -0700

    Display numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * add - adds two number
+ * @a: first argumnet
+ * @b: second argument
+ * Return: Always 0.
+ */
+void add(int a, int b)
+{
+	return (a + b);
+}
