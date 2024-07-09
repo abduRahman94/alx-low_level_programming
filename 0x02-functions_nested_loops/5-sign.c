9b500dadbb782fb273b071a55a2bab1af139b302
abduRahman94 <sabdouseye@gmail.com>
2024-07-08 18:09:06 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * print_sign - Returns sign of given argument
+ * @c: the char to check
+ * Return: Always 0.
+ */
+int print_sign(int c)
+{
+	if (c > 0)
+	{
+		printf('+');
+		return (1);
+	}
+	else if (c < 0)
+	{
+		printf('-');
+		return (-1);
+	else
+	{
+		printf('0');
+		return (0);
+	}
+}
