5e45ef77bee22eab0733aa6d8f5cdbab7a7e3200
abduRahman94 <sabdouseye@gmail.com>
2024-07-08 23:52:52 -0700

    update times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,13 +10,13 @@
 void times_table(void)
 {
 	int i, j, product;
-	
+
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 			product = i * j;
-			
+
 			if (j != 0)
 			{
 				_putchar(',');
@@ -32,7 +32,7 @@ void times_table(void)
 			}
 			_putchar((product % 10) + '0');
 		}
-		
+
 		_putchar('\n');
 	}
 }
