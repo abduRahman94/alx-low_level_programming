8ea66c0c37b71c96bf11fc738c0c1577ea294b28
abduRahman94 <sabdouseye@gmail.com>
2024-07-09 00:16:36 -0700

    Print times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,13 +12,11 @@ void print_times_table(int n)
 	{
 		return;
 	}
-
 	for (i = 0; i <= n; i++)
 	{
 		for (j = 0; j <= n; j++)
 		{
 			product = i * j;
-
 			if (j != 0)
 			{
 				_putchar(',');
@@ -33,21 +31,17 @@ void print_times_table(int n)
 					_putchar(' ');
 				}
 			}
-
 			if (product >= 100)
 			{
 				_putchar((product / 100) + '0');
 				_putchar(((product / 10) % 10) + '0');
 			}
-
 			else if (product >= 10)
 			{
 				_putchar((product / 10) + '0');
 			}
-
 			_putchar((product % 10) + '0');
 		}
-
 		_putchar('\n');
 	}
 }
