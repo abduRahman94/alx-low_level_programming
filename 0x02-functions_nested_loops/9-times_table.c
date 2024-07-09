f1ebac8285931c3893fcac2e31a12b7428fd2a84
abduRahman94 <sabdouseye@gmail.com>
2024-07-08 19:35:18 -0700

    Display numbers

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,28 +8,27 @@
  */
 void times_table(void)
 {
-    int i, j, result;
-
-    for (i = 0; i <= 9; i++)
-    {
-        for (j = 0; j <= 9; j++)
-        {
-            result = i * j;
-            if (j == 0)
-                _putchar('0');
-            else
-                _putchar(' ');
-
-            if (result <= 9)
-                _putchar(' ');
-            else
-                _putchar((result / 10) + '0');
-
-            _putchar((result % 10) + '0');
-
-            if (j < 9)
-                _putchar(',');
-        }
-        _putchar('\n');
-    }
+	int i, j, result;
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			result = i * j;
+			if (j == 0)
+				_putchar('0');
+			else
+				_putchar(' ');
+			
+			if (result <= 9)
+				_putchar(' ');
+			else
+				_putchar((result / 10) + '0');
+			
+			_putchar((result % 10) + '0');
+			
+			if (j < 9)
+				_putchar(',');
+		}
+		_putchar('\n');
+	}
 }
