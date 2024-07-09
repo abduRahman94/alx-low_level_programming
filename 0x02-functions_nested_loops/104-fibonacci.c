6bbe146702b84e71aaed10dabf71209c8ce02dd4
abduRahman94 <sabdouseye@gmail.com>
2024-07-09 01:23:48 -0700

    updated fibonacci sequence

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,44 +7,45 @@
  */
 int main(void)
 {
-    unsigned long a = 1, b = 2;
-    unsigned long a_high, a_low, b_high, b_low;
-    unsigned long high, low;
-    int count;
+	unsigned long a = 1, b = 2;
+	unsigned long a_high, a_low, b_high, b_low;
+	unsigned long high, low;
+	int count;
 
-    printf("%lu, %lu", a, b);
+	printf("%lu, %lu", a, b);
 
-    for (count = 2; count < 92; count++)
-    {
-        unsigned long next = a + b;
-        printf(", %lu", next);
-        a = b;
-        b = next;
-    }
+	for (count = 2; count < 92; count++)
+	{
+		unsigned long next = a + b;
+		printf(", %lu", next);
+		a = b;
+		b = next;
+	}
 
-    a_high = a / 10000000000UL;
-    a_low = a % 10000000000UL;
-    b_high = b / 10000000000UL;
-    b_low = b % 10000000000UL;
+	a_high = a / 10000000000UL;
+	a_low = a % 10000000000UL;
+	b_high = b / 10000000000UL;
+	b_low = b % 10000000000UL;
 
-    for (; count < 98; count++)
-    {
-        high = a_high + b_high;
-        low = a_low + b_low;
-        if (low >= 10000000000UL)
-        {
-            high += 1;
-            low %= 10000000000UL;
-        }
+	for (; count < 98; count++)
+	{
+		high = a_high + b_high;
+		low = a_low + b_low;
 
-        printf(", %lu%010lu", high, low);
+		if (low >= 10000000000UL)
+		{
+			high += 1;
+			low %= 10000000000UL;
+		}
 
-        a_high = b_high;
-        a_low = b_low;
-        b_high = high;
-        b_low = low;
-    }
+		printf(", %lu%010lu", high, low);
 
-    printf("\n");
-    return (0);
+		a_high = b_high;
+		a_low = b_low;
+		b_high = high;
+		b_low = low;
+	}
+
+	printf("\n");
+	return (0);
 }
